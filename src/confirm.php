d0a90c0795269c3a20f9bcdc71d32449d37fc669
jeanpi <johnpaulwallway@gmail.com>
2013-03-09 16:16:24 -0800

    Adding db correspondence

--- src/confirm.php
+++ src/confirm.php
@@ -1,6 +1,6 @@
 <!--
 	Peer Review U 
-  BetaByte webDev Team:  
+  BetaByte webDev Team: Anthony C. Hopkins, Sohaib Hussain, Paul Park Kwon, John Paul Wallway 
 	CSE 360
 	Confirm.php just adds the info passed by signup.php into the text file containing
 	everyone's records
